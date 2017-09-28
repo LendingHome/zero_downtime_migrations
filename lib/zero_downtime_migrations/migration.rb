86d3f8defb24617208fc1f012e020e8b0bf4f80c
Eric Chan <eric.chan@socialchorus.com>
2017-09-28 10:44:09 -0700

    Throw error if column is added without disabling ddl transaction

--- lib/zero_downtime_migrations/migration.rb
+++ lib/zero_downtime_migrations/migration.rb
@@ -29,6 +29,7 @@ module ZeroDowntimeMigrations
       Migration.data = false
       Migration.ddl = false
       Migration.index = false
+      Migration.column = false
       Migration.safe ||= old_migration? || reverse_migration? || rollup_migration?
 
       super.tap do
@@ -76,9 +77,14 @@ module ZeroDowntimeMigrations
       %i(add_index).include?(method)
     end
 
+    def column_method?(method)
+      %i(add_column rename_column).include?(method)
+    end
+
     def method_missing(method, *args)
       Migration.ddl = true if ddl_method?(method)
       Migration.index = true if index_method?(method)
+      Migration.column = true if column_method?(method)
       validate(method, *args)
       super
     end
--- spec/zero_downtime_migrations/validation/ddl_migration_spec.rb
+++ spec/zero_downtime_migrations/validation/ddl_migration_spec.rb
@@ -1,11 +1,9 @@
 RSpec.describe ZeroDowntimeMigrations::Validation::DdlMigration do
   let(:error) { ZeroDowntimeMigrations::UnsafeMigrationError }
 
-  context "with a migration that adds a column with ddl disabled" do
+  context "with a migration that adds a column without ddl disabled" do
     let(:migration) do
       Class.new(ActiveRecord::Migration[5.0]) do
-        disable_ddl_transaction!
-
         def change
           add_column :users, :active, :boolean
         end
@@ -14,6 +12,22 @@ RSpec.describe ZeroDowntimeMigrations::Validation::DdlMigration do
 
     it "raises an unsafe migration error" do
       expect { migration.migrate(:up) }.to raise_error(error)
+    end
+  end
+  
+  context "with a migration that adds a column with ddl disabled" do
+    let(:migration) do
+      Class.new(ActiveRecord::Migration[5.0]) do
+        disable_ddl_transaction!
+        
+        def change
+          add_column :users, :active, :boolean
+        end
+      end
+    end
+
+    it "does not raise an unsafe migration error" do
+      expect { migration.migrate(:up) }.to_not raise_error(error)
     end
   end
 
